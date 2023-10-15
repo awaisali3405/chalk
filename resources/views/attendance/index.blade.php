73ee5d79e26098374f9751fad17f1d82fe080fcf
ali awan <awaisali3405@gmail.com>
2023-10-15 16:32:44 +0500

    udpate

---
+++ resources/views/attendance/index.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+@section('content')
+    <div class="content-body">
+        <!-- row -->
+        <div class="container-fluid">
+
+            <div class="row page-titles mx-0">
+                <div class="col-sm-6 p-md-0">
+                    <div class="welcome-text">
+                        <h4>All Academic Calender</h4>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row tab-content">
+                        <div id="list-view" class="tab-pane fade active show col-lg-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title">All Academic Calender </h4>
+                                    <a href="{{ route('academicCalender.create') }}" class="btn btn-primary">+ Add new</a>
+                                </div>
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <table id="example5" class="display" style="min-width: 845px">
+                                            <thead>
+                                                <tr>
+                                                    <th>Sr</th>
+                                                    <th>Session</th>
+                                                    <th>Week</th>
+                                                    <th>Status</th>
+
+                                                    <th>Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($academicCalender as $key => $value)
+                                                    <tr>
+                                                        <td>{{ $key + 1 }}</td>
+
+                                                        <td>{{ $value->start_date }} - {{ $value->end_date }}</td>
+                                                        <td>Week
+                                                            {{ \Carbon\Carbon::parse($value->start_date)->diffInWeeks(\Carbon\Carbon::parse($value->end_date)->addDay(1)) }}
+                                                        </td>
+                                                        <td>{{ $value->active ? 'Active' : 'De Active' }}</td>
+                                                        <td>
+                                                            <a href="{{ route('academicCalender.edit', $value->id) }}"
+                                                                class="btn btn-sm btn-primary"><i
+                                                                    class="la la-pencil"></i></a>
+                                                            <a href="{{ route('academicCalender.show', $value->id) }}"
+                                                                class="btn btn-sm btn-primary">{{ $value->active ? 'De Active' : 'Active' }}</a>
+                                                            {{-- <a href="{{ route('branch.show', $value->id) }}"
+                                                                class="btn btn-sm btn-danger"><i
+                                                                    class="la la-trash-o"></i></a> --}}
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
--- resources/views/enquiry/index.blade.php
+++ resources/views/enquiry/index.blade.php
@@ -10,11 +10,11 @@
                         <h4>All Enquiry</h4>
                     </div>
                 </div>
-               
+
             </div>
 
             <div class="row">
-             
+
                 <div class="col-lg-12">
                     <div class="row tab-content">
                         <div id="list-view" class="tab-pane fade active show col-lg-12">
@@ -51,21 +51,34 @@
                                                             {{ \Carbon\Carbon::parse(auth()->user()->session()->start_date)->diffInWeeks(\Carbon\Carbon::parse($value->enquriy)->addDay(1)) }}
                                                         </td>
                                                         <td>
-                                                            <a href="{{ route('enquiry.edit', $value->id) }}" title="edit"
-                                                                class="btn btn-sm btn-primary"><i
+                                                            <button type="button" class="btn btn-primary dropdown-toggle"
+                                                                data-toggle="dropdown" aria-expanded="true">
+                                                                Action
+                                                            </button>
+                                                            <div class="dropdown-menu" x-placement="bottom-start"
+                                                                style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 36px, 0px);">
+                                                                <a class="dropdown-item"
+                                                                    href="{{ route('enquiry.edit', $value->id) }}">Edit</a>
+                                                                <a class="dropdown-item"
+                                                                    href="{{ route('enquiry.note', $value->id) }}">Note</a>
+                                                                <a class="dropdown-item"
+                                                                    href="{{ route('enquiry.upload', $value->id) }}">Upload</a>
+                                                                <a class="dropdown-item"
+                                                                    href="{{ route('enquiry.register', $value->id) }}">Register</a>
+                                                                <a class="dropdown-item"
+                                                                    href="{{ route('enquiry.edit', $value->id) }}">Delete</a>
+                                                            </div>
+                                                            {{-- <a title="edit" class="btn btn-sm btn-primary"><i
                                                                     class="la la-pencil"></i></a>
-                                                            <a href="{{ route('enquiry.note', $value->id) }}" title="note"
-                                                                class="btn btn-sm btn-secondary"><i
+                                                            <a title="note" class="btn btn-sm btn-secondary"><i
                                                                     class="la la-sticky-note"></i></a>
-                                                            <a href="{{ route('enquiry.upload', $value->id) }}"
-                                                                title="upload" class="btn btn-sm btn-info"><i
+                                                            <a title="upload" class="btn btn-sm btn-info"><i
                                                                     class="la la-arrow-circle-up"></i></a>
-                                                            <a href="{{ route('enquiry.register', $value->id) }}"
-                                                                title="note" class="btn btn-sm btn-info"><i
+                                                            <a title="note" class="btn btn-sm btn-info"><i
                                                                     class="la la-registered"></i></a>
                                                             <a href="{{ route('enquiry.edit', $value->id) }}"
                                                                 title="note" class="btn btn-sm btn-danger"><i
-                                                                    class="la la-trash-o"></i></a>
+                                                                    class="la la-trash-o"></i></a> --}}
                                                             {{-- <a href="{{ route('branch.show', $value->id) }}"
                                                                 class="btn btn-sm btn-danger"><i
                                                                     class="la la-trash-o"></i></a> --}}
