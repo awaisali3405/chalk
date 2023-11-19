ae39454d772b4e2f58ffe36a9a6bf3689791a31e
obada <awaisali3405@gmail.com>
2023-11-19 17:28:16 +0500

    commit

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -582,7 +582,7 @@ class StudentsController extends Controller
         $string = '<option value="">-</option>';
         // dd($year);
         foreach ($year->student as $key => $value) {
-
+            
             $string .= "<option value='" . $value->id . "'>" . $value->first_name . "</option>";
         }
         return response()->json(['data' => $string]);
