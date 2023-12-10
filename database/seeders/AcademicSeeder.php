e68f5298558485bad5c13877032a41fcd3caa95d
awaisali3405 <awaisali3405@gmail.com>
2023-12-10 17:39:23 +0500

    commit

--- database/seeders/AcademicSeeder.php
+++ database/seeders/AcademicSeeder.php
@@ -18,14 +18,14 @@ class AcademicSeeder extends Seeder
         // 29-8-2022 - 03-09-2023
         AcademicCalender::insert([
             [
-                'start_date' => Carbon::create('2023', '09', '04'),
-                'end_date' =>  Carbon::create('2024', '09', '01'),
-                'active' => 1
-            ],
-            [
                 'start_date' => Carbon::create('2022', '08', '29'),
                 'end_date' =>  Carbon::create('2023', '09', '03'),
                 'active' => 0
+            ],
+            [
+                'start_date' => Carbon::create('2023', '09', '04'),
+                'end_date' =>  Carbon::create('2024', '09', '01'),
+                'active' => 1
             ],
 
 
