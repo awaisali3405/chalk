755ee4986018c264f66eb7f4b6cdf0deb2ae46ff
awaisali3405 <awaisali3405@gmail.com>
2024-01-06 19:57:08 +0500

    c

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -7,7 +7,7 @@
             <div class="row page-titles mx-0">
                 <div class="col-sm-6 p-md-0">
                     <div class="welcome-text">
-                        <h4>All Product</h4>
+                        <h4>Book Stock</h4>
                     </div>
                 </div>
 
@@ -20,7 +20,7 @@
                         <div id="list-view" class="tab-pane fade active show col-lg-12">
                             <div class="card">
                                 <div class="card-header">
-                                    <h4 class="card-title">All Product </h4>
+                                    <h4 class="card-title">Book Stock</h4>
                                     <a href="{{ route('product.create') }}" class="btn btn-primary">+ Add new</a>
                                 </div>
                                 <div class="card-body">
@@ -66,9 +66,6 @@
                                                                     class="la la-pencil"></i></a>
                                                             <a href="{{ route('product.transfer', $value->id) }}"
                                                                 class="btn btn-primary">Transfer Product</a>
-                                                            {{-- <a href="{{ route('year.show', $value->id) }}"
-                                                                class="btn btn-sm btn-danger"><i
-                                                                    class="la la-trash-o"></i></a> --}}
                                                         </td>
                                                     </tr>
                                                 @endforeach
