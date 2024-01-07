0b7f682a85de586536354605c42ff878bf741377
awaisali3405 <awaisali3405@gmail.com>
2024-01-07 22:15:07 +0500

    cc

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -96,7 +96,7 @@ class Staff extends Model
     }
     public function salary($from_date, $to_date)
     {
-        return $this->receipt()->where('date', '>=', $from_date)->where('date', '<=', $to_date)->sum('salary');
+        return $this->receipt()->where('date', '>=', $from_date)->where('date', '<=', $to_date)->sum('salary') - $this->receipt()->where('date', '>=', $from_date)->where('date', '<=', $to_date)->sum('deduction');
     }
     public function employerNI($from_date, $to_date)
     {
